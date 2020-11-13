e8ddd25a10425bf4ff72255ce33b1cd8addc9f51
Vijay Rajasekar <vijayrajasekar1@gmail.com>
2020-11-13 02:38:55 -0500

    Mega Menu done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,29 +6,29 @@ import MegaMenu from "../MegaMenu"
 
 function Header() : JSX.Element {
     return (
-        <div className="d-flex align-content-center justify-content-xl-center shadow">
-            <Navbar className="d-flex header-container header" expand="lg">
-                <Navbar.Brand className="d-flex"><img src={teradyneLogo} /></Navbar.Brand>
+        <div className="d-flex align-content-center shadow justify-content-lg-center">
+            <Navbar className="d-lg-flex header-container header" expand="lg">
+                <Navbar.Brand className="d-lg-flex"><img src={teradyneLogo} /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse className="d-lg-flex row-lg justify-content-lg-end" id="basic-navbar-nav">
                     <Nav>
                         <NavDropdown className="d-lg-flex header-link col-lg cols-2 has-megamenu" title={<span className="header-link">Products</span>} id="products-nav-dropdown">
-                            <NavDropdown.Item className="shadow"><MegaMenu /></NavDropdown.Item>
+                            <NavDropdown.ItemText className="shadow"><MegaMenu title="Products" /></NavDropdown.ItemText>
                         </NavDropdown>
-                        <NavDropdown className="d-lg-flex header-link col-lg cols-2" title={<span className="header-link">Industries</span>} id="industries-nav-dropdown">
-                            <NavDropdown.Header></NavDropdown.Header>
+                        <NavDropdown className="d-lg-flex header-link col-lg cols-2 has-megamenu" title={<span className="header-link">Industries</span>} id="industries-nav-dropdown">
+                            <NavDropdown.ItemText className="shadow"><MegaMenu title="Industries" /></NavDropdown.ItemText>
                         </NavDropdown>
-                        <NavDropdown className="d-lg-flex header-link col-lg cols-2" title={<span className="header-link">Applications</span>} id="Applications-nav-dropdown">
-                            <NavDropdown.Header></NavDropdown.Header>
+                        <NavDropdown className="d-lg-flex header-link col-lg cols-2 has-megamenu" title={<span className="header-link">Applications</span>} id="Applications-nav-dropdown">
+                            <NavDropdown.ItemText className="shadow"><MegaMenu title="Applications" /></NavDropdown.ItemText>
                         </NavDropdown>
-                        <NavDropdown className="d-lg-flex header-link col-lg cols-2" title={<span className="header-link">Services</span>} id="Services-nav-dropdown">
-                            <NavDropdown.Header></NavDropdown.Header>
+                        <NavDropdown className="d-lg-flex header-link col-lg cols-2 has-megamenu" title={<span className="header-link">Services</span>} id="Services-nav-dropdown">
+                            <NavDropdown.ItemText className="shadow"><MegaMenu title="Services" /></NavDropdown.ItemText>
                         </NavDropdown>
-                        <NavDropdown className="d-lg-flex header-link col-lg cols-2" title={<span className="header-link">Support</span>} id="support-nav-dropdown">
-                            <NavDropdown.Header></NavDropdown.Header>
+                        <NavDropdown className="d-lg-flex header-link col-lg cols-2 has-megamenu" title={<span className="header-link">Support</span>} id="support-nav-dropdown">
+                            <NavDropdown.ItemText className="shadow"><MegaMenu title="Support" /></NavDropdown.ItemText>
                         </NavDropdown>
-                        <NavDropdown className="d-lg-flex header-link col-lg cols-2" title={<span className="header-link">Company</span>} id="company-nav-dropdown">
-                            <NavDropdown.Header></NavDropdown.Header>
+                        <NavDropdown className="d-lg-flex header-link col-lg cols-2 has-megamenu" title={<span className="header-link">Company</span>} id="company-nav-dropdown">
+                            <NavDropdown.ItemText className="shadow"><MegaMenu title="Company" /></NavDropdown.ItemText>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
--- src/components/MegaMenu/index.tsx
+++ src/components/MegaMenu/index.tsx
@@ -1,25 +1,69 @@
 import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
-
+import { Container, Row, Col, Card } from "react-bootstrap"
+import whitePaperCardImg from "./whitepaper.png"
 import "./MegaMenu.css"
 
-function MegaMenu(): JSX.Element {
+interface ComponentWithTitleProp {
+    title: string
+}
+
+function ProductSnippet({title}: ComponentWithTitleProp): JSX.Element {
+    return (
+        <Col className="my-3" xs={12} lg={6}>
+            <div className="product-snippet-title">{title}</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id libero.</div>
+        </Col>
+    )
+}
+
+function MegaMenu({title}: ComponentWithTitleProp): JSX.Element {
     return (
         <Container className="justify-content-start megamenu-container" fluid>
-            <Row>
-                <Col>
-                    <div className="title">Products</div>
-                    <div className="product">Industrial Automation</div>
-                    <div className="product">Digital/Mixed Signal Testing</div>
-                    <div className="product">Industrial Automation</div>
-                    <div className="product">Industrial Automation</div>
-                    <div className="product">Industrial Automation</div>
-                    <div className="product">Industrial Automation</div>
-                    <div className="product">Industrial Automation</div>
+            <Row className="w-100">
+                <Col lg={3} className="py-5 h-100 bg-white products-container">
+                    <div className="title">{title}</div>
+                    <div className="product"><a href ="" onClick={(e) => e.preventDefault()} className="product-name">Industrial Automation</a></div>
+                    <div className="product"><a href ="" onClick={(e) => e.preventDefault()} className="product-name">Digital/Mixed Signal Testing</a></div>
+                    <div className="product"><a href ="" onClick={(e) => e.preventDefault()} className="product-name">Linear, Power &amp; Automotive Testing</a></div>
+                    <div className="product"><a href ="" onClick={(e) => e.preventDefault()} className="product-name">Micro-Controller / Image Sensor Testing</a></div>
+                    <div className="product"><a href ="" onClick={(e) => e.preventDefault()} className="product-name">Printed Circuit Board Testing</a></div>
+                    <div className="product"><a href ="" onClick={(e) => e.preventDefault()} className="product-name">Storage System / Level Testing</a></div>
+                    <div className="product"><a href ="" onClick={(e) => e.preventDefault()} className="product-name">Wireless Testing</a></div>
+                    <div className="product"><a href ="" onClick={(e) => e.preventDefault()} className="product-name">Defense &amp; Aerospace</a></div>
                 </Col>
-                <Col>
+                <Col lg={6} className="product-content px-5 py-5">
+                    <Row className="flex-lg-wrap">
+                        <ProductSnippet title="Test Station Handler" />
+                        <ProductSnippet title="Test Station LX / LX2" />
+                        <ProductSnippet title="Test Station Handler Large Format" />
+                        <ProductSnippet title="Test Station Asynchronous" />
+                        <ProductSnippet title="Test Station Station In-Line" />
+                        <ProductSnippet title="Test Station Online" />
+                        <ProductSnippet title="Test Station LHS" />
+                    </Row>
                 </Col>
-                <Col>
+                <Col lg={3} className="product-highlight px-5 py-5">
+                    <Card className="shadow">
+                        <Card.Img src={whitePaperCardImg} />
+                        <Card.Body>
+                            <Card.Title className="card-title">
+                                <div className="purple white-paper my-3">
+                                    WHITE PAPER
+                                </div>
+                                <div>
+                                    The Importance of Testing
+                                </div>
+                            </Card.Title>
+                            <Card.Text>
+                                <div>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id libero. Lorem ipsum dolor sit amet
+                                </div>
+                                <div className="mt-4">
+                                    <a className="white-paper-link" href="" onClick={(e) => e.preventDefault()} >Get the whitepaper</a>
+                                </div>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                 </Col>
             </Row>
         </Container>
