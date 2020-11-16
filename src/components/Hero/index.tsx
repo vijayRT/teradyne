229de9161c04eab87dbd338a746fcb36baeabdcf
Vijay Rajasekar <vijayrajasekar1@gmail.com>
2020-11-15 21:06:21 -0500

    trying to figure out 4k

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,10 +1,12 @@
-import React from "react"
-import { Button, Col, Row } from "react-bootstrap"
-
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React, { useState } from "react"
+import { Button, Col, Row, Accordion, Card } from "react-bootstrap"
 import "./Hero.css"
 function Hero(): JSX.Element {
+    const [opened, setOpened] = useState(false)
     return (
-        <div className="d-flex hero">
+        <div className="d-flex hero justify-content-center">
             <Row className="hero-text-container align-content-center px-5">
                 <Col lg={6}>
                     <div>
@@ -15,6 +17,28 @@ function Hero(): JSX.Element {
                     </div>
                 </Col>
             </Row>
+            <Accordion className="brands-drawer bg-white shadow">
+                <Card>
+                    <Card.Header>
+                        <Row>
+                            <Col className="teradyne-companies text-center justify-content-center" xs={2}><h5 className="teradyne-companies-text">TERADYNE COMPANIES</h5></Col>
+                            <Col xs={10} className="teradyne-companies-description my-auto">
+                                <h6 className="font-weight-bold">Teradyne companies provide lorem ipsum...</h6>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh </p>
+
+                                <Accordion.Toggle className="view-companies" as={Button} variant="link" eventKey="0">
+                                        VIEW TERADYNE COMPANIES <FontAwesomeIcon icon={faCaretDown} />
+                                </Accordion.Toggle>
+                            </Col>
+                        </Row>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="0">
+                        <Card.Body>Random Content</Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+            </Accordion>
+
+
         </div>
     )
 }
